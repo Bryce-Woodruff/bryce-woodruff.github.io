96c7fbc0ed7ede6df888fadc25eeb0392cd4f1e0
Bryce-Woodruff <146599868+Bryce-Woodruff@users.noreply.github.com>
2023-12-07 00:46:11 -0600

    Add files via upload

---
+++ External_Project_part2/scripts/script.js
@@ -0,0 +1,8 @@
+/*
+    Student Name:Bryce Woodruff
+    File Name: script.js
+*/
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
